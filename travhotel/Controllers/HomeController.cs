646eb2c85b0e1d085eb1f3e3ef12f6bf84b645d0
f4m <33976043+f4m@users.noreply.github.com>
2020-08-24 14:58:44 +0600

    First commit

---
+++ travhotel/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace travhotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+        public ActionResult LoginTraveler()
+        {
+            return View();
+        }
+        public ActionResult LoginOwner()
+        {
+            return View();
+        }
+        public ActionResult RegisterAdmin()
+        {
+            return View();
+        }
+        public ActionResult RegisterTraveler()
+        {
+            return View();
+        }
+        public ActionResult RegisterOwner()
+        {
+            return View();
+        }
+    }
+}
